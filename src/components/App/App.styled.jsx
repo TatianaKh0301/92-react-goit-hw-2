ef841a204bd4c47032b0fe3beb61546306d800d0
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-12-15 08:20:22 +0200

    Update App.styled.jsx

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
     height: 100vh;
-    padding: 50px;
-    color: #ffffff;
+    padding: 100px;
+    /* color: #ffffff; */
     background-color: bisque;
 `;
